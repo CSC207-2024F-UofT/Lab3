f671b13feb6b075b2e3ef0be0b538a0edf511417
meldy04 <120117788+meldy04@users.noreply.github.com>
2024-09-23 16:14:22 -0400

    Added in spanish

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -27,7 +27,7 @@ public class InLabByHandTranslator implements Translator {
     public List<String> getCountryLanguages(String country) {
         // Checkstyle: The String "can" appears 4 times in the file.
         if (CANADA.equals(country)) {
-            return new ArrayList<>(List.of("de", "en", "zh"));
+            return new ArrayList<>(List.of("de", "en", "zh", "es"));
         }
         return new ArrayList<>();
     }
@@ -66,6 +66,9 @@ public class InLabByHandTranslator implements Translator {
         }
         else if ("zh".equals(language)) {
             x = "加拿大";
+        }
+        else if ("es".equals(language)) {
+            x = "Canadá";
         }
         else {
             x = null;
