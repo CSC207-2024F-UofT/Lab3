099419d0afa186b978a84c5c856500e607019572
AnthonyInGithub <97834700+AnthonyInGithub@users.noreply.github.com>
2024-09-23 19:52:05 -0400

    first two file checkstyle update

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -21,17 +21,19 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+    public static final String CANADA = "can";
+
     @Override
     public List<String> getCountryLanguages(String country) {
         // TODO Checkstyle: The String "can" appears 4 times in the file.
-        if ("can".equals(country)) {
+        if (CANADA.equals(country)) {
             return new ArrayList<>(List.of("de", "en", "zh"));
         }
         return new ArrayList<>();
     }
 
     // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
+
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -41,7 +43,7 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public List<String> getCountries() {
-        return new ArrayList<>(List.of("can"));
+        return new ArrayList<>(List.of(CANADA));
     }
 
     /**
@@ -55,13 +57,13 @@ public class InLabByHandTranslator implements Translator {
     public String translate(String country, String language) {
         // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
         // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!country.equals("can")) {
+        if (!country.equals(CANADA)) {
             return null;
         }
-        if (language.equals("de")) {
+        if ("de".equals(language)) {
             return "Kanada";
         }
-        else if (language.equals("en")) {
+        else if ("en".equals(language)) {
             return "Canada";
         }
         else if ("zh".equals(language)) {
