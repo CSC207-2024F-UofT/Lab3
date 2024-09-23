7f1e1967f0f272d4bf582b3269088666b9a6e071
JqrqmyChqn <chanjeremy2015@gmail.com>
2024-09-23 12:18:59 -0400

    new changes

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
+//change
 /**
  * A minimal example of reading and using the JSON data from resources/sample.json.
  */
