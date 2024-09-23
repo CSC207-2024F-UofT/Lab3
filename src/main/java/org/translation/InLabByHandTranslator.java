69b14fd8e33afdf80f9f94897dbed9a0236abc72
Franco Cabral <francomaricabral@gmail.com>
2024-09-23 17:44:24 -0400

    Fixed checkstyle for countrycodeconverter and inlabbyhandtranslator

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 /**
  * An implementation of the Translator interface which translates
- * the country code "can" to several languages.
+ * the country code CANADA to several languages.
  */
 public class InLabByHandTranslator implements Translator {
     /**
@@ -21,17 +21,16 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+
+    public static final String CANADA = "can";
+
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Checkstyle: The String "can" appears 4 times in the file.
-        if ("can".equals(country)) {
+        if (CANADA.equals(country)) {
             return new ArrayList<>(List.of("de", "en", "zh"));
         }
         return new ArrayList<>();
     }
-
-    // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -41,7 +40,7 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public List<String> getCountries() {
-        return new ArrayList<>(List.of("can"));
+        return new ArrayList<>(List.of(CANADA));
     }
 
     /**
@@ -53,22 +52,21 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public String translate(String country, String language) {
-        // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!country.equals("can")) {
+        if (!CANADA.equals(country)) {
             return null;
         }
-        if (language.equals("de")) {
-            return "Kanada";
+
+        String translation = null;
+        if ("de".equals(language)) {
+            translation = "Kanada";
         }
-        else if (language.equals("en")) {
-            return "Canada";
+        else if ("en".equals(language)) {
+            translation = "Canada";
         }
         else if ("zh".equals(language)) {
-            return "加拿大";
+            translation = "加拿大";
         }
-        else {
-            return null;
-        }
+
+        return translation;
     }
 }
