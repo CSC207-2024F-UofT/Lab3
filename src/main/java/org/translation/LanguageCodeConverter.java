a629393bfa104664e5da14e1d15c7628549f1db8
Denis <denis.rivard@mail.utoronto.ca>
2024-09-23 16:36:53 -0400

    Finished LanguageCodeConverter

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -12,8 +13,9 @@ import java.util.Map;
  * This class provides the service of converting language codes to their names.
  */
 public class LanguageCodeConverter {
-
-    // TODO Task: pick appropriate instance variables to store the data necessary for this class
+    private Map langToCode;
+    private Map codeToLang;
+    private int numLanguages;
 
     /**
      * Default constructor which will load the language codes from "language-codes.txt"
@@ -33,12 +35,22 @@ public class LanguageCodeConverter {
         try {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
-
-            // TODO Task: use lines to populate the instance variable
-            //           tip: you might find it convenient to create an iterator using lines.iterator()
-
-        // TODO Checkstyle: '}' on next line should be alone on a line.
-        } catch (IOException | URISyntaxException ex) {
+            Iterator<String> it = lines.iterator();
+            it.next();
+            langToCode = new HashMap();
+            codeToLang = new HashMap();
+            int countLangs = 0;
+            while (it.hasNext()) {
+                String[] aCountry = it.next().split("\t");
+                String lan = aCountry[0];
+                String code = aCountry[1];
+                langToCode.put(lan, code);
+                codeToLang.put(code, lan);
+                countLangs++;
+            }
+            numLanguages = countLangs;
+        }
+        catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
 
@@ -50,8 +62,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return code;
+        return codeToLang.get(code).toString();
     }
 
     /**
@@ -60,8 +71,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return language;
+        return langToCode.get(language).toString();
     }
 
     /**
@@ -69,7 +79,6 @@ public class LanguageCodeConverter {
      * @return how many languages are included in this code converter.
      */
     public int getNumLanguages() {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return 0;
+        return numLanguages;
     }
 }
