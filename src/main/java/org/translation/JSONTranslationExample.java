719c55b4e939cf6b4c41cd0eb6abe07afbb233b3
marburcea <maria.burcea@mail.utoronto.ca>
2024-09-23 15:00:31 -0400

    to do changes

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -36,8 +36,8 @@ public class JSONTranslationExample {
      */
     public String getCanadaCountryNameSpanishTranslation() {
 
-        // TODO Checkstyle: '30' is a magic number.
-        JSONObject canada = jsonArray.getJSONObject(30);
+        final int thirty = 30;
+        JSONObject canada = jsonArray.getJSONObject(thirty);
         return canada.getString("es");
     }
 
