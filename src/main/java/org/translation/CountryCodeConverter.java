ffba9c7cb094c241faf3157598b98d3a7888fc93
cbolinius <cooperbolinius@gmail.com>
2024-09-23 17:03:13 -0400

    Lab 3 TODOS

--- src/main/java/org/translation/CountryCodeConverter.java
+++ src/main/java/org/translation/CountryCodeConverter.java
@@ -4,16 +4,24 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * This class provides the service of converting country codes to their names.
  */
 public class CountryCodeConverter {
 
-    // TODO Task: pick appropriate instance variable(s) to store the data necessary for this class
+    private HashMap<String,List<String>[]> countryDictionary = new HashMap<>();
+    /*
+    // A list of the country names in this CountryCodeConverter
+    private List<String> countryNames = new ArrayList<String>();
+    // A list of the country Alpha-2 codes in this CountryCodeConverter
+    private List<String> alpha2Codes = new ArrayList<String>();
+    // A list of the country Alpha-3 codes in this CountryCodeConverter
+    private List<String> alpha3Codes = new ArrayList<String>();
+    // A list of the country numeric codes in this CountryCodeConverter
+    private List<String> numericCodes = new ArrayList<String>();
+    */
 
     /**
      * Default constructor which will load the country codes from "country-codes.txt"
@@ -35,6 +43,17 @@ public class CountryCodeConverter {
                     .getClassLoader().getResource(filename).toURI()));
 
             // TODO Task: use lines to populate the instance variable(s)
+            for (String line : lines) {
+                String[] values = line.split("\t");
+                List<String> temp = Arrays.asList(new String[]{values[1], values[2], values[3]});
+                this.countryDictionary.put(values[0], temp]);
+                /*this.countryNames.add(values[0]);
+                this.alpha2Codes.add(values[1]);
+                this.alpha3Codes.add(values[2]);
+                this.numericCodes.add(values[3]);
+                 */
+            }
+
 
         }
         catch (IOException | URISyntaxException ex) {
