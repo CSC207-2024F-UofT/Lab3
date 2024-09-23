c8bacf7170c78799dae245be94bc14450a723300
five <mikawu73@gmail.com>
2024-09-23 19:37:57 -0400

    1.1

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -21,6 +21,8 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+    public static final String CANADA = "can";
+
     @Override
     public List<String> getCountryLanguages(String country) {
         // TODO Checkstyle: The String "can" appears 4 times in the file.
@@ -29,9 +31,6 @@ public class InLabByHandTranslator implements Translator {
         }
         return new ArrayList<>();
     }
-
-    // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -54,14 +53,14 @@ public class InLabByHandTranslator implements Translator {
     @Override
     public String translate(String country, String language) {
         // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!country.equals("can")) {
+        String value;
+        if (!"can".equals(country)) {
             return null;
         }
-        if (language.equals("de")) {
+        if ("de".equals(language)) {
             return "Kanada";
         }
-        else if (language.equals("en")) {
+        else if ("en".equals(language)) {
             return "Canada";
         }
         else if ("zh".equals(language)) {
