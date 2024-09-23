f055c300fce3bfcc1e5856fcc6a0f044556b2b6e
Safeer Ahmed <safeer.ahmed@mail.utoronto.ca>
2024-09-23 14:20:22 -0400

    fixed the styles error

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -21,17 +21,15 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+    private final String canada = "can";
+
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Checkstyle: The String "can" appears 4 times in the file.
-        if ("can".equals(country)) {
+        if (canada.equals(country)) {
             return new ArrayList<>(List.of("de", "en", "zh"));
         }
         return new ArrayList<>();
     }
-
-    // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -41,7 +39,7 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public List<String> getCountries() {
-        return new ArrayList<>(List.of("can"));
+        return new ArrayList<>(List.of(canada));
     }
 
     /**
@@ -53,22 +51,23 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public String translate(String country, String language) {
-        // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!country.equals("can")) {
-            return null;
+        String name = "";
+
+        //        if (!country.equals(Canada12)) {
+        //            return null;
+        //        }
+        if ("de".equals(language)) {
+            name = "Kanada";
         }
-        if (language.equals("de")) {
-            return "Kanada";
-        }
-        else if (language.equals("en")) {
-            return "Canada";
+        else if ("en".equals(language)) {
+            name = "Canada";
         }
         else if ("zh".equals(language)) {
-            return "加拿大";
+            name = "加拿大";
         }
-        else {
-            return null;
-        }
+        //        else {
+        //            return null;
+        //        }
+        return name;
     }
 }
