6590bd4f9e620041b86537696a38157f4b84d19d
Anthonybonev <tony@Anthonys-MacBook-Air-4.local>
2024-09-23 18:38:59 -0400

    TODO done

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -3,10 +3,6 @@ package org.translation;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO Task: modify this class so that it also supports the Spanish language code "es" and
-//            one more language code of your choice. Each member of your group should add
-//            support for one additional language code on a branch; then push and create a pull request on GitHub.
-
 // Extra Task: if your group has extra time, you can add support for another country code in this class.
 
 /**
@@ -27,7 +23,7 @@ public class InLabByHandTranslator implements Translator {
     @Override
     public List<String> getCountryLanguages(String country) {
         if (CANADA.equals(country)) {
-            return new ArrayList<>(List.of("de", "en", "zh"));
+            return new ArrayList<>(List.of("de", "en", "zh", "es", "fa", "hy"));
         }
         return new ArrayList<>();
     }
@@ -62,6 +58,15 @@ public class InLabByHandTranslator implements Translator {
             }
             else if ("zh".equals(language)) {
                 result = "加拿大";
+            }
+            else if ("es".equals(language)) {
+                result = "Canadá";
+            }
+            else if ("fa".equals(language)) {
+                result = "کانادا";
+            }
+            else if ("hy".equals(language)) {
+                result = "Կանադա";
             }
             return result;
         }
