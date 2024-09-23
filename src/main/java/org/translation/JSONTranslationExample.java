4462e1aef9ea2ac84352c2fd79adcaad0083e77a
KUNYU47 <kwanjyu.li@mail.utoronto.ca>
2024-09-23 16:20:57 -0400

    refactored to extract helper method

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -21,8 +21,6 @@ public class JSONTranslationExample {
         try {
             // this next line of code reads in a file from the resources folder as a String,
             // which we then create a new JSONArray object from.
-            // TODO CheckStyle: Line is longer than 120 characters
-            //                  (note: you can split a line such that the next line starts with a .method()... call
             String jsonString = Files.readString(
                     Paths.get(getClass().getClassLoader().getResource("sample.json").toURI()));
             this.jsonArray = new JSONArray(jsonString);
@@ -53,6 +51,14 @@ public class JSONTranslationExample {
      * @return the translation of country to the given language or "Country not found" if there is no translation.
      */
     public String getCountryNameTranslation(String countryCode, String languageCode) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            final JSONObject jsonObject = jsonArray.getJSONObject(i);
+            final String currCode = jsonObject.getString("alpha3");
+            if (countryCode.equals(currCode)) {
+                return jsonObject.getString(languageCode);
+            }
+        }
+
         return "Country not found";
     }
 
