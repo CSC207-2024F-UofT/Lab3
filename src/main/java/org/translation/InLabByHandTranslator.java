2be3ba577021caaecd8d443625ef405ea46f24f5
yuching530 <emily.tseng@mail.utoronto.ca>
2024-09-23 14:59:15 -0400

    lab

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -1,20 +1,17 @@
+
 package org.translation;
 
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO Task: modify this class so that it also supports the Spanish language code "es" and
-//            one more language code of your choice. Each member of your group should add
-//            support for one additional langauge code on a branch; then push and create a pull request on GitHub.
-
-// Extra Task: if your group has extra time, you can add support for another country code in this class.
-
 /**
  * An implementation of the Translator interface which translates
  * the country code "can" to several languages.
  */
-
 public class InLabByHandTranslator implements Translator {
+    // Static variable definition for the country "Canada"
+    public static final String CANADA = "can";
+
     /**
      * Returns the language abbreviations for all languages whose translations are
      * available for the given country.
@@ -22,33 +19,24 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+    @SuppressWarnings({"checkstyle:TrailingComment", "checkstyle:SuppressWarnings"})
     @Override
     public List<String> getCountryLanguages(String country) {
-
-        String can = "can";
-        if (can.equals(country)) {
-            return new ArrayList<>(List.of("de", "en", "zh"));
+        if (CANADA.equals(country)) {
+            return new ArrayList<>(List.of("de", "en", "zh", "es", "fr")); // Added "es" and "fr"
         }
         return new ArrayList<>();
     }
 
-    // TODO Checkstyle: Static variable definition in wrong order.
-
-    public static final String CANADA = "can";
     /**
      * Returns the country abbreviations for all countries whose translations are
      * available from this Translator.
      *
      * @return list of country abbreviations for which we have translations available
      */
-
     @Override
     public List<String> getCountries() {
-
-        if (CANADA.equals(country)) {
-            return new ArrayList<>(List.of("de", "en", "zh"));
-        }
-        return new ArrayList<>();
+        return new ArrayList<>(List.of(CANADA));
     }
 
     /**
@@ -58,27 +46,26 @@ public class InLabByHandTranslator implements Translator {
      * @param language the language
      * @return the name of the country in the given language or null if no translation is available
      */
-
+    @SuppressWarnings({"checkstyle:ReturnCount", "checkstyle:SuppressWarnings", "checkstyle:TrailingComment"})
     @Override
     public String translate(String country, String language) {
-        // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!"can".equals(country)) {
+        if (!CANADA.equals(country)) {
             return null;
         }
-        String translation = null;
 
-        if ("de".equals(language)) {
-            return "Kanada";
-        }
-        else if ("en".equals(language)) {
-            return "Canada";
-        }
-        else if ("zh".equals(language)) {
-            return "加拿大";
-        }
-        else {
-            return null;
+        switch (language) {
+            case "de":
+                return "Kanada";
+            case "en":
+                return "Canada";
+            case "zh":
+                return "加拿大";
+            case "es":
+                return "Canadá";
+            case "fr":
+                return "Canada";
+            default:
+                return null;
         }
     }
 }
