426369db49937608de9a51615caf88cd7339c953
five <mikawu73@gmail.com>
2024-09-24 11:29:58 -0400

    InLabByHandTranslator Todo solved

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -3,10 +3,6 @@ package org.translation;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO Task: modify this class so that it also supports the Spanish language code "es" and
-//            one more language code of your choice. Each member of your group should add
-//            support for one additional langauge code on a branch; then push and create a pull request on GitHub.
-
 // Extra Task: if your group has extra time, you can add support for another country code in this class.
 
 /**
@@ -25,9 +21,8 @@ public class InLabByHandTranslator implements Translator {
 
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Checkstyle: The String "can" appears 4 times in the file.
         if (CANADA.equals(country)) {
-            return new ArrayList<>(List.of("de", "en", "zh"));
+            return new ArrayList<>(List.of("de", "en", "zh", "es"));
         }
         return new ArrayList<>();
     }
@@ -54,7 +49,7 @@ public class InLabByHandTranslator implements Translator {
     public String translate(String country, String language) {
         String value;
         if (!CANADA.equals(country)) {
-            value = null;
+            return null;
         }
         if ("de".equals(language)) {
             value = "Kanada";
@@ -64,6 +59,9 @@ public class InLabByHandTranslator implements Translator {
         }
         else if ("zh".equals(language)) {
             value = "加拿大";
+        }
+        else if ("es".equals(language)) {
+            value = "Canadá";
         }
         else {
             value = null;
