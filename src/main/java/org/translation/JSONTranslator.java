427e4e03cabef5d08b9852539c4997492c8bfb8b
guoyuc11 <aliceyuchen.guo@mail.utoronto.ca>
2024-09-26 00:13:39 -0400

    finished JSONTranslator.java

--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -1,22 +1,22 @@
 package org.translation;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * An implementation of the Translator interface which reads in the translation
  * data from a JSON file. The data is read in once each time an instance of this class is constructed.
  */
 public class JSONTranslator implements Translator {
-
-    // TODO Task: pick appropriate instance variables for this class
-
+    private Map<String, Map<String, String>> countriesToLang;
+    private Map<String, List<String>> countriesToLangCode;
+    private List<String> countries;
     /**
      * Constructs a JSONTranslator using data from the sample.json resources file.
      */
@@ -29,6 +29,7 @@ public class JSONTranslator implements Translator {
      * @param filename the name of the file in resources to load the data from
      * @throws RuntimeException if the resource file can't be loaded properly
      */
+    @SuppressWarnings("checkstyle:MagicNumber")
     public JSONTranslator(String filename) {
         // read the file to get the data to populate things...
         try {
@@ -37,10 +38,33 @@ public class JSONTranslator implements Translator {
 
             JSONArray jsonArray = new JSONArray(jsonString);
 
-            // TODO Task: use the data in the jsonArray to populate your instance variables
-            //            Note: this will likely be one of the most substantial pieces of code you write in this lab.
+            countriesToLang = new HashMap<>();
+            countriesToLangCode = new HashMap<>();
+            countries = new ArrayList<>();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject country = jsonArray.getJSONObject(i);
+                String countryCode = country.getString("alpha3");
+                countries.add(countryCode);
 
+                List<String> keysList = new ArrayList<>();
+                for (int j = 0; j < country.length(); j++) {
+                    Iterator<String> keys = country.keys();
+                    while (keys.hasNext()) {
+                        String key = keys.next();
+                        keysList.add(key);
+                    }
+                }
+                countriesToLangCode.put(countryCode, keysList);
+
+                Map<String, String> languages = new HashMap<>();
+                for (int k = 3; k < country.length(); k++) {
+                    String langKey = keysList.get(k);
+                    languages.put(langKey, country.getString(langKey));
+                }
+                countriesToLang.put(countryCode, languages);
+            }
         }
+
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
@@ -48,21 +72,16 @@ public class JSONTranslator implements Translator {
 
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Task: return an appropriate list of language codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        return this.countriesToLangCode.get(country);
     }
 
     @Override
     public List<String> getCountries() {
-        // TODO Task: return an appropriate list of country codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        return this.countries;
     }
 
     @Override
     public String translate(String country, String language) {
-        // TODO Task: complete this method using your instance variables as needed
-        return null;
+        return this.countriesToLang.get(country).get(language);
     }
 }
