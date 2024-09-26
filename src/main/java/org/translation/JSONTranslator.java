1c969770ef979f47875b69b12cf3b5ff289c51de
KseniaTk <ksenia.tkaczyk@mail.utoronto.ca>
2024-09-26 09:04:41 -0400

    modified constructor

--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import javax.swing.text.html.HTMLDocument;
 
 /**
  * An implementation of the Translator interface which reads in the translation
@@ -47,8 +48,10 @@ public class JSONTranslator implements Translator {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 ids.add(jsonObject.getInt("id"));
                 countryCodes.add(jsonObject.getString("alpha3"));
+                Set<String> allKeys = jsonObject.keySet();
 
             }
+
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
