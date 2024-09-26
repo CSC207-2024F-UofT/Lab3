c749692ede66a12462c78d0b298b48312213d096
kzyanj <kensleyzhouyanjun023@gmail.com>
2024-09-25 20:51:12 -0400

    InLab CheckStyle Complete

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -21,17 +21,16 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+    public static final String CANADA = "can";
+
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Checkstyle: The String "can" appears 4 times in the file.
-        if ("can".equals(country)) {
+        // store "can in a constant and reference it."
+        if (CANADA.equals(country)) {
             return new ArrayList<>(List.of("de", "en", "zh"));
         }
         return new ArrayList<>();
     }
-
-    // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -53,22 +52,23 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public String translate(String country, String language) {
-        // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!country.equals("can")) {
-            return null;
+        // create String to store the return value
+        String storeReturn = "";
+        if (!country.equals(CANADA)) {
+            storeReturn = null;
         }
-        if (language.equals("de")) {
-            return "Kanada";
+        if ("de".equals(language)) {
+            storeReturn = "Kanada";
         }
-        else if (language.equals("en")) {
-            return "Canada";
+        else if ("en".equals(language)) {
+            storeReturn = "Canada";
         }
         else if ("zh".equals(language)) {
-            return "加拿大";
+            storeReturn = "加拿大";
         }
         else {
-            return null;
+            storeReturn = null;
         }
+        return storeReturn;
     }
 }
