dcc50665bc24613324ebe7a69760ef28aff753c7
Zev G <godfreyzev@gmail.com>
2024-09-26 10:54:30 -0400

    fixed style errors

--- src/main/java/org/translation/Main.java
+++ src/main/java/org/translation/Main.java
@@ -14,6 +14,8 @@ import java.util.Scanner;
  */
 public class Main {
 
+    private static final String QUIT_KEYWORD = "quit";
+
     /**
      * This is the main entry point of our Translation System!<br/>
      * A class implementing the Translator interface is created and passed into a call to runProgram.
@@ -39,16 +41,14 @@ public class Main {
     public static void runProgram(Translator translator) {
         while (true) {
             String country = promptForCountry(translator);
-            // TODO CheckStyle: The String "quit" appears 3 times in the file.
-            // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-            if (country.equals("quit")) {
+            if (QUIT_KEYWORD.equals(country)) {
                 break;
             }
             // TODO Task: Once you switch promptForCountry so that it returns the country
             //            name rather than the 3-letter country code, you will need to
             //            convert it back to its 3-letter country code when calling promptForLanguage
             String language = promptForLanguage(translator, country);
-            if (language.equals("quit")) {
+            if (language.equals(QUIT_KEYWORD)) {
                 break;
             }
             // TODO Task: Once you switch promptForLanguage so that it returns the language
@@ -61,7 +61,7 @@ public class Main {
             Scanner s = new Scanner(System.in);
             String textTyped = s.nextLine();
 
-            if ("quit".equals(textTyped)) {
+            if (QUIT_KEYWORD.equals(textTyped)) {
                 break;
             }
         }
