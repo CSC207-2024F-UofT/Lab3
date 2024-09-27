14f5ea70e55c891655da1b1a986cc368365a503b
MaxNguyen5040 <MaxNguyen5040>
2024-09-26 22:53:39 -0400

    maybe finished LanguageCodeConverter?

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -12,7 +12,7 @@ import java.util.*;
  */
 public class LanguageCodeConverter {
 
-    public static Map<String,String> country_codes = new HashMap<>();
+    public static Map<String,String> language_codes = new HashMap<>();
 
     /**
      * Default constructor which will load the language codes from "language-codes.txt"
@@ -44,11 +44,11 @@ public class LanguageCodeConverter {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
                 if (parts.length == 2) {
-                    country_codes.put(parts[0], parts[1]);
+                    language_codes.put(parts[0], parts[1]);
                 } else {
                     String[] countries = Arrays.copyOfRange(parts, 0, parts.length - 1);
                     String country_strings = String.join(" ", countries);
-                    country_codes.put(country_strings, parts[country_codes.size() - 1]);
+                    language_codes.put(country_strings, parts[language_codes.size() - 1]);
                 }
             }
         }
@@ -64,8 +64,12 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return code;
+        for(Map.Entry<String,String> entry : language_codes.entrySet()){
+            if(entry.getValue().equals(code)){
+                return entry.getKey();
+            }
+        }
+        return "N/A";
     }
 
     /**
@@ -74,8 +78,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return language;
+        return language_codes.getOrDefault(language, "code not found");
     }
 
     /**
@@ -83,7 +86,6 @@ public class LanguageCodeConverter {
      * @return how many languages are included in this code converter.
      */
     public int getNumLanguages() {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return 0;
+        return language_codes.size();
     }
 }
