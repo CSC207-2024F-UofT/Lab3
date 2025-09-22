876de1ea6317b807b334628c6e1963fea1a34ad9
SS <cubixularhelix@gmail.com>
2025-09-22 18:36:24 -0400

    taskA

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -45,7 +45,6 @@ public class LanguageCodeConverter {
                 String[] tokens = line.split("\t");
                 languageCodeToLanguage.put(tokens[1], tokens[0]);
                 languageToLanguageCode.put(tokens[0], tokens[1]);
-                // TODO Task A: use line to populate the instance variables
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -59,9 +58,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        code = languageToLanguageCode.get(code);
-        return code;
+        return languageCodeToLanguage.get(code);
     }
 
     /**
@@ -70,9 +67,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        language = languageCodeToLanguage.get(language);
-        return language;
+        return languageToLanguageCode.get(language);
     }
 
     /**
