810713a19d59d5df7da4978cf440731cf91dabbd
Chris <chris.mong@mail.utoronto.ca>
2025-09-22 18:01:26 -0400

    Completed Task A

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,9 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
+                String[] parts = line.split("\\s+", 2);
+                languageCodeToLanguage.put(parts[1].trim(), parts[0].trim());
+                languageToLanguageCode.put(parts[0].trim(), parts[1].trim());
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -56,7 +58,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
+        code = this.languageCodeToLanguage.get(code);
         return code;
     }
 
@@ -66,7 +68,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
+        language = this.languageToLanguageCode.get(language);
         return language;
     }
 
