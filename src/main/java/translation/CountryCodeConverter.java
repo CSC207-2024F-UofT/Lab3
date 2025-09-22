15894b8d337cf18f901b66f224199c2d77f68e0b
Tai Poole <tai.poole314@gmail.com>
2025-09-22 15:31:55 -0400

    Implemented methods for CountryCodeConverter (task B)

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -41,7 +41,10 @@ public class CountryCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
-                // TODO Task B: use parts to populate the instance variables
+                String countryName = parts[0];
+                String countryCode = parts[2];
+                this.countryCodeToCountry.put(countryCode, countryName);
+                this.countryToCountryCode.put(countryName, countryCode);
             }
         }
         catch (IOException | URISyntaxException ex) {
@@ -56,8 +59,7 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return code;
+        return this.countryCodeToCountry.get(code);
     }
 
     /**
@@ -66,8 +68,7 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return country;
+        return this.countryToCountryCode.get(country);
     }
 
     /**
@@ -75,7 +76,6 @@ public class CountryCodeConverter {
      * @return how many countries are included in this country code converter.
      */
     public int getNumCountries() {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return 0;
+        return this.countryCodeToCountry.size();
     }
 }
