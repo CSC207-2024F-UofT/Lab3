79c5cb4a52a05c9bdea29e34b453709c1371977f
0NCEbot <danbhnguyen2006@gmail.com>
2025-09-22 14:08:43 -0400

    completed fromLanguageCode and fromLanguage functions (1)

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -56,7 +56,6 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageCodeToLanguage.get(code);
     }
 
@@ -66,7 +65,6 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageToLanguageCode.get(language);
     }
 
