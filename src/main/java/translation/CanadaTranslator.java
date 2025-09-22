5c1e2997f0b3da45cb5a2a71d9f163d556225b3a
samario <samario.liu@mail.utoronto.ca>
2025-09-22 17:49:59 -0400

    else branches

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -22,7 +22,7 @@ public class CanadaTranslator implements Translator {
      */
     @Override
     public List<String> getLanguageCodes() {
-        return new ArrayList<>(List.of("de", "en", "zh"));
+        return new ArrayList<>(List.of("de", "en", "zh", "es"));
     }
 
     /**
@@ -53,6 +53,9 @@ public class CanadaTranslator implements Translator {
         }
         else if (languageCode.equals("en")) {
             return "Canada";
+        }
+        else if (languageCode.equals("es")) {
+            return "Canadá";
         }
         else if ("zh".equals(languageCode)) {
             return "加拿大";
