f1aa2a48c07cb33ef9f3527268b7705013380a3d
Saladin Shaurov <salah@SS-Macbook-Air.local>
2025-09-22 17:50:40 -0400

    Implemented Task 1 with es and fr

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -22,7 +22,7 @@ public class CanadaTranslator implements Translator {
      */
     @Override
     public List<String> getLanguageCodes() {
-        return new ArrayList<>(List.of("de", "en", "zh"));
+        return new ArrayList<>(List.of("de", "en", "zh", "es", "fr"));
     }
 
     /**
@@ -54,8 +54,14 @@ public class CanadaTranslator implements Translator {
         else if (languageCode.equals("en")) {
             return "Canada";
         }
-        else if ("zh".equals(languageCode)) {
+        else if (languageCode.equals("zh")) {
             return "加拿大";
+        }
+        else if (languageCode.equals("es")) {
+            return "Canada";
+        }
+        else if (languageCode.equals("fr")) {
+            return "Canadá";
         }
         else {
             return null;
