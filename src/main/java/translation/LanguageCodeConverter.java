c5c9d7651d4d008d6a02f40fb039d76f9b4de227
cengqilin <468027952@qq.com>
2025-09-22 15:36:15 -0400

    Update LanguageCodeConverter

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -65,7 +65,6 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         String language = languageCodeToLanguage.get(code);
         return language;
     }
@@ -76,7 +75,6 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         String code = languageToLanguageCode.get(language);
         return code;
     }
