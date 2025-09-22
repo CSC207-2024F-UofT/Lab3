df648d55fa3f5e436ffd6860e1dc087b0a4bf3cd
jadenal-aidroos <jjtortoise@icloud.com>
2025-09-22 18:11:07 -0400

    Fixed CountryCodeConverter.java, (TASK C) - Jaden and Nate

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -41,7 +41,10 @@ public class CountryCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
-                // TODO Task B: use parts to populate the instance variables
+                this.countryToCountryCode.put(parts[0], parts[2]);
+                this.countryCodeToCountry.put(parts[2], parts[0]);
+
+
             }
         }
         catch (IOException | URISyntaxException ex) {
@@ -56,8 +59,7 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return code;
+        return this.countryCodeToCountry.get(code.toUpperCase());
     }
 
     /**
@@ -66,8 +68,7 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return country;
+        return this.countryToCountryCode.get(country);
     }
 
     /**
@@ -75,7 +76,6 @@ public class CountryCodeConverter {
      * @return how many countries are included in this country code converter.
      */
     public int getNumCountries() {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return 0;
+        return this.countryCodeToCountry.size();
     }
 }
