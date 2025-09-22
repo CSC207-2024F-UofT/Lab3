12812198014084a96e18e52b7203375abf30dee7
berkebasak <berke.basak@mail.utoronto.ca>
2025-09-22 18:14:45 -0400

    task-a is fully done

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -49,7 +49,6 @@ public class LanguageCodeConverter {
                     languageCodeToLanguage.put(code, language);
                     languageToLanguageCode.put(language, code);
                 }
-                // TODO Task A: use line to populate the instance variables
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -63,7 +62,6 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageCodeToLanguage.get(code);
 
     }
@@ -74,7 +72,6 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageToLanguageCode.get(language);
     }
 
