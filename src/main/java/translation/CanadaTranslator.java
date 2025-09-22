e34405dd3d582dd064c87ce11e41f4b2eb632fe4
Isabella Zhong <isabellazhong888@gmail.com>
2025-09-22 11:35:39 -0400

    add spanish and japan to canada translator"

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -3,10 +3,6 @@ package translation;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO Task 1: as a team, update this class so that it also supports the Spanish language code "es" and
-//              one more language code of your team's choice. Submit a PR once the code is working and
-//              make sure everyone has a local working copy of the code.
-
 /**
  * An implementation of the Translator interface that translates
  * the country code "can" to several languages.
@@ -56,6 +52,11 @@ public class CanadaTranslator implements Translator {
         }
         else if ("zh".equals(languageCode)) {
             return "加拿大";
+        }
+        else if(languageCode.equals("es")) {
+            return "Canadá";
+        } else if(languageCode.equals("jp")) {
+            return "カナダ";
         }
         else {
             return null;
