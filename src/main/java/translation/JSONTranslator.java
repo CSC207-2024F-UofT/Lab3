ffe23e1ec8292d4e83fb9e4099cea1caffd4438d
Rein McKelway <rein.mckelway@mail.utoronto.ca>
2025-09-22 12:31:55 -0400

    working A and C tasks

--- src/main/java/translation/JSONTranslator.java
+++ src/main/java/translation/JSONTranslator.java
@@ -24,6 +24,7 @@ public class JSONTranslator implements Translator {
 
     // the key used is "countryCode-languageCode"; the value is the translated country name
     private final Map<String, String> translations = new HashMap<>();
+
     /**
      * Construct a JSONTranslator using data from the sample.json resources file.
      */
@@ -33,6 +34,7 @@ public class JSONTranslator implements Translator {
 
     /**
      * Construct a JSONTranslator populated using data from the specified resources file.
+     *
      * @param filename the name of the file in resources to load the data from
      * @throws RuntimeException if the resource file can't be loaded properly
      */
@@ -51,30 +53,30 @@ public class JSONTranslator implements Translator {
 
                 List<String> languages = new ArrayList<>();
 
-                // TODO Task C: record this countryCode in the correct instance variable
-
+                countryCodes.add(countryCode);
                 // iterate through the other keys to get the information that we need
                 for (String key : countryData.keySet()) {
                     if (!key.equals("id") && !key.equals("alpha2") && !key.equals("alpha3")) {
                         String languageCode = key;
-                        // TODO Task C: record this translation in the appropriate instance variable
-
-                        if (!languages.contains(languageCode)) {
+                        String translation = countryData.getString(key);
+                        translations.put(countryCode+"-"+languageCode, translation);
+                        if(!languageCodes.contains(languageCode)) {
+                            languageCodes.add(languageCode);
+                        }
+                        if(!languages.contains(languageCode)) {
                             languages.add(languageCode);
                         }
                     }
                 }
             }
-        }
-        catch (IOException | URISyntaxException ex) {
+        } catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
     }
 
     @Override
     public List<String> getLanguageCodes() {
-        // TODO Task C: return a copy of the language codes
-        return new ArrayList<>();
+        return this.languageCodes;
     }
 
     @Override
@@ -84,7 +86,6 @@ public class JSONTranslator implements Translator {
 
     @Override
     public String translate(String countryCode, String languageCode) {
-        // TODO Task C: complete this method using your instance variables as needed
-        return "JSONTranslator's translate method is not implemented!";
+        return translations.get(countryCode+"-"+languageCode);
     }
 }
