57f5adbaac38fb497242b1d1d718cc664071f0fa
Zihan Lu <wilsonlzh@foxmail.com>
2025-09-22 17:38:26 -0400

    add the supports for Spanish.

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -22,7 +22,7 @@ public class CanadaTranslator implements Translator {
      */
     @Override
     public List<String> getLanguageCodes() {
-        return new ArrayList<>(List.of("de", "en", "zh"));
+        return new ArrayList<>(List.of("de", "en", "zh", "es"));
     }
 
     /**
@@ -56,6 +56,9 @@ public class CanadaTranslator implements Translator {
         }
         else if ("zh".equals(languageCode)) {
             return "加拿大";
+        }
+        else if ("es".equals(languageCode)) {
+            return "Canadá";
         }
         else {
             return null;
