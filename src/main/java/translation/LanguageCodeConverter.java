b09d66b1fb2ad2748349b6ae3faa5f7ba45e27fe
Ale Benavides Loo <albenaloo27@gmail.com>
2025-09-22 18:25:07 -0400

    task A complete

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,8 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                languageCodeToLanguage.put(line.split("\t")[0], line.split("\t")[1]);
+                String[] x = line.split("\t");
+                languageCodeToLanguage.put(x[1], x[0]);
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -56,7 +57,6 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageCodeToLanguage.get(code);
     }
 
@@ -66,7 +66,6 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         languageToLanguageCode.get(language);
         return language;
     }
