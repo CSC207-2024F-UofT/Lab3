84b2ac2dfd8733a150c0103e10fdf1d68243aa59
a-kursunoglu <ateskursunoglu2@gmail.com>
2025-09-22 18:31:10 -0400

    Finish task C

--- src/main/java/translation/JSONTranslator.java
+++ src/main/java/translation/JSONTranslator.java
@@ -52,15 +52,23 @@ public class JSONTranslator implements Translator {
                 List<String> languages = new ArrayList<>();
 
                 // TODO Task C: record this countryCode in the correct instance variable
+                if (!countryCodes.contains(countryCode)) {
+                    countryCodes.add(countryCode);
+                }
 
                 // iterate through the other keys to get the information that we need
                 for (String key : countryData.keySet()) {
                     if (!key.equals("id") && !key.equals("alpha2") && !key.equals("alpha3")) {
                         String languageCode = key;
                         // TODO Task C: record this translation in the appropriate instance variable
+                        String translation = countryData.getString(languageCode);
+                        translations.put(countryCode + "-" + languageCode, translation);
 
                         if (!languages.contains(languageCode)) {
                             languages.add(languageCode);
+                        }
+                        if (!languageCodes.contains(languageCode)) {
+                            languageCodes.add(languageCode);
                         }
                     }
                 }
@@ -74,7 +82,7 @@ public class JSONTranslator implements Translator {
     @Override
     public List<String> getLanguageCodes() {
         // TODO Task C: return a copy of the language codes
-        return new ArrayList<>();
+        return new ArrayList<>(languageCodes);
     }
 
     @Override
@@ -85,6 +93,7 @@ public class JSONTranslator implements Translator {
     @Override
     public String translate(String countryCode, String languageCode) {
         // TODO Task C: complete this method using your instance variables as needed
-        return "JSONTranslator's translate method is not implemented!";
+        return translations.get(countryCode + "-" + languageCode);
+
     }
 }
