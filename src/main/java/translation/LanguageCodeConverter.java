6b7f25afcbdc8d15bcfc72230da8b233b461eddb
Milan White <milan.white.25@gmail.com>
2025-09-22 09:57:31 -0400

    feat: Completed Task A, solving issue #1

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -21,7 +21,7 @@ public class LanguageCodeConverter {
 
     /**
      * Default constructor that loads the language codes from "language-codes.txt"
-     * in the resources folder.
+     * in the folder.
      */
     public LanguageCodeConverter() {
         this("language-codes.txt");
@@ -42,7 +42,14 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
+
+                // Task A: use line to populate the instance variables
+                String[] parts = line.split("\t");
+
+
+                languageCodeToLanguage.put(parts[1], parts[0]);
+                languageToLanguageCode.put(parts[0], parts[1]);
+
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -56,8 +63,9 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return code;
+        // Task A: update this code to use the correct instance variable to return the appropriate value
+
+        return languageCodeToLanguage.get(code);
     }
 
     /**
@@ -66,8 +74,8 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        // Task A: update this code to use the correct instance variable to return the appropriate value
+        return languageToLanguageCode.get(language);
     }
 
     /**
