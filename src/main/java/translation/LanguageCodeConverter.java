e8c9766976cfc8a1bb4ca4128abf44cd64e2d3da
kwanyinlai <laikwanyin06@gmail.com>
2025-09-22 18:07:51 -0400

    partial fix to bug

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,9 +42,8 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                String[] parts = line.split("\\s+");
-                String language = parts[0];
-                String languageCode = parts[1];
+                String language = line.substring(0, line.length()-2);
+                String languageCode = line.substring(line.length()-2);
                 languageCodeToLanguage.put(language, languageCode);
                 languageToLanguageCode.put(languageCode, language);
             }
@@ -60,7 +59,6 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageCodeToLanguage.get(code);
     }
 
@@ -70,8 +68,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        return languageToLanguageCode.get(language);
     }
 
     /**
