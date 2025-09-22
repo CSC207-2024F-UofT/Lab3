be7d5cf2d8b43877152facd30f7ddf118cc5541a
shidydx <shiley.kane@mail.utoronto.ca>
2025-09-22 17:27:06 -0400

    task 1

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -22,7 +22,7 @@ public class CanadaTranslator implements Translator {
      */
     @Override
     public List<String> getLanguageCodes() {
-        return new ArrayList<>(List.of("de", "en", "zh"));
+        return new ArrayList<>(List.of("de", "en", "zh", "es", "fr"));
     }
 
     /**
@@ -56,6 +56,12 @@ public class CanadaTranslator implements Translator {
         }
         else if ("zh".equals(languageCode)) {
             return "加拿大";
+        }
+        else if ("es".equals(languageCode)) {
+            return "Canadá";
+        }
+        else if ("fr".equals(languageCode)) {
+            return "Canada";
         }
         else {
             return null;
