6e0699858e798fec21bf8e785281f00e690467e9
Xicco <sammuel.karya@gmail.com>
2025-09-22 14:06:13 -0400

    DELETED TO-DO's

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -57,9 +57,6 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-
-
         return languageCodeToLanguage.get(code);
     }
 
@@ -69,7 +66,6 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageToLanguageCode.get(language);
     }
 
