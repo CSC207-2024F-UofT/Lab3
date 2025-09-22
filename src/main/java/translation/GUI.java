7bdf651516ce72479ba411c7864dbd53b811b444
stingythrower <maoladina@gmail.com>
2025-09-22 18:07:40 -0400

    Fixed country codes to country name

--- src/main/java/translation/GUI.java
+++ src/main/java/translation/GUI.java
@@ -3,6 +3,7 @@ package translation;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.concurrent.Callable;
 
 
 // TODO Task D: Update the GUI for the program to align with UI shown in the README example.
@@ -14,10 +15,11 @@ public class GUI {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JPanel languagePanel = new JPanel();
-            languagePanel.add(new JLabel("Language:"));
             Translator translator = new JSONTranslator();
             LanguageCodeConverter languageCodeConverter = new LanguageCodeConverter();
+            CountryCodeConverter countryCodeConverter = new CountryCodeConverter();
+            JPanel languagePanel = new JPanel();
+            languagePanel.add(new JLabel("Language:"));
             JComboBox<String> languageComboBox = new JComboBox<>();
             for(String countryCode : translator.getLanguageCodes()) {
                 languageComboBox.addItem(languageCodeConverter.fromLanguageCode(countryCode));
@@ -28,10 +30,10 @@ public class GUI {
             JPanel countryPanel = new JPanel();
             countryPanel.setLayout(new GridLayout(0, 2));
             countryPanel.add(new JLabel("Language:"), 0);
-            String[] items = new String[translator.getLanguageCodes().size()];
+            String[] items = new String[translator.getCountryCodes().size()];
             int i = 0;
-            for(String langaugeCode : translator.getLanguageCodes()) {
-                items[i++] = langaugeCode;
+            for(String countryCode : translator.getCountryCodes()) {
+                items[i++] = countryCodeConverter.fromCountryCode(countryCode);
             }
             // create the JList with the array of strings and set it to allow multiple
             // items to be selected at once.
@@ -43,8 +45,8 @@ public class GUI {
             countryPanel.add(scrollPane, 1);
 
             JPanel buttonPanel = new JPanel();
-            JButton submit = new JButton("Submit");
-            buttonPanel.add(submit);
+            // JButton submit = new JButton("Submit");
+            //buttonPanel.add(submit);
 
             JLabel resultLabelText = new JLabel("Translation:");
             buttonPanel.add(resultLabelText);
@@ -53,7 +55,7 @@ public class GUI {
 
 
             // adding listener for when the user clicks the submit button
-            submit.addActionListener(new ActionListener() {
+            languageComboBox.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     //String language = languageField.getText();
@@ -61,7 +63,7 @@ public class GUI {
 
                     // for now, just using our simple translator, but
                     // we'll need to use the real JSON version later.
-                    Translator translator = new CanadaTranslator();
+
 
                     //String result = translator.translate(country, language);
                     /*if (result == null) {
@@ -76,6 +78,7 @@ public class GUI {
             JPanel mainPanel = new JPanel();
             mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
             mainPanel.add(languagePanel);
+            mainPanel.add(buttonPanel);
             mainPanel.add(countryPanel);
 
             JFrame frame = new JFrame("Country Name Translator");
