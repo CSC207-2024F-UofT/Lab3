112e6cfaddbd4533d700660c33d5889879c2aad0
rananagash <rana.nagash@mail.utoronto.ca>
2025-09-22 16:58:15 -0400

    task d: not done

--- src/main/java/translation/GUI.java
+++ src/main/java/translation/GUI.java
@@ -13,17 +13,26 @@ public class GUI {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JPanel countryPanel = new JPanel();
-            JTextField countryField = new JTextField(10);
-            countryField.setText("can");
-            countryField.setEditable(false); // we only support the "can" country code for now
-            countryPanel.add(new JLabel("Country:"));
-            countryPanel.add(countryField);
-
             JPanel languagePanel = new JPanel();
-            JTextField languageField = new JTextField(10);
+//            JTextField countryField = new JTextField(10);
+            // countryField.setText("can");
+            // countryField.setEditable(false); // we only support the "can" country code for now
             languagePanel.add(new JLabel("Language:"));
-            languagePanel.add(languageField);
+            Translator translator = new JSONTranslator();
+
+            // create combobox, add country codes into it, and add it to our panel
+            JComboBox<String> languageComboBox = new JComboBox<>();
+            for(String countryCode : translator.getLanguageCodes()) {
+                languageComboBox.addItem(countryCode);
+            }
+            languagePanel.add(languageComboBox);
+
+            // countryPanel.add(countryField);
+
+            JPanel translationPanel = new JPanel();
+            JTextField languageField = new JTextField(10);
+            translationPanel.add(new JLabel("Translation:"));
+            translationPanel.add(languageField);
 
             JPanel buttonPanel = new JPanel();
             JButton submit = new JButton("Submit");
@@ -40,11 +49,11 @@ public class GUI {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     String language = languageField.getText();
-                    String country = countryField.getText();
+                    String country = languageComboBox.getSelectedItem().toString();
 
                     // for now, just using our simple translator, but
                     // we'll need to use the real JSON version later.
-                    Translator translator = new CanadaTranslator();
+                    Translator translator = new JSONTranslator();
 
                     String result = translator.translate(country, language);
                     if (result == null) {
@@ -58,8 +67,8 @@ public class GUI {
 
             JPanel mainPanel = new JPanel();
             mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-            mainPanel.add(countryPanel);
             mainPanel.add(languagePanel);
+            mainPanel.add(translationPanel);
             mainPanel.add(buttonPanel);
 
             JFrame frame = new JFrame("Country Name Translator");
