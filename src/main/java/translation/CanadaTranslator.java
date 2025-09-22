f0e467e32f900127e4fbe4c39f87357421259590
franklinlu <wanglu5613@icloud.com>
2025-09-22 13:37:37 -0400

    add bg

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -56,8 +56,8 @@ public class CanadaTranslator implements Translator {
         }
         else if ("zh".equals(languageCode)) {
             return "加拿大";
-        } else if (languageCode.equals("zh-TW")) {
-            return "加拿大";
+        } else if (languageCode.equals("bg")) {
+            return "Канада";
         } else if (languageCode.equals("es")) {
             return "Canadá";
         } else {
