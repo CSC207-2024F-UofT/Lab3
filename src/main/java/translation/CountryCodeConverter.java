daf187f0c4ccdc994e390f7c6329c74c0f74c5cc
Perseus-D <pereus@PERSEUS-4.local>
2025-09-22 16:43:09 -0400

    Fixed task B

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -41,8 +41,8 @@ public class CountryCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
-                countryCodeToCountry.put(parts[0], parts[2]);
-                countryToCountryCode.put(parts[2], parts[0]);
+                countryCodeToCountry.put(parts[2], parts[0]);
+                countryToCountryCode.put(parts[0], parts[2]);
             }
         }
         catch (IOException | URISyntaxException ex) {
