b6b7372596c4e052bd3fe3c27f34e990e62564dc
Pepper256 <splotchyscar@gmail.com>
2025-09-22 18:59:03 -0400

    fix implementation error of fromCountry in CountryCodeConverter.java implement GUI satisfying README.md

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -68,7 +68,7 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        return countryCodeToCountry.get(country);
+        return countryToCountryCode.get(country);
     }
 
     /**
