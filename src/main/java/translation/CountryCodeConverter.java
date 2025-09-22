12a793b85b2d22bc8e40175c5d7d70fd97c36552
anton <antonmcassidy@gmail.com>
2025-09-22 16:04:56 -0400

    Task B Initial

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -41,7 +41,11 @@ public class CountryCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
-                // TODO Task B: use parts to populate the instance variables
+                countryCodeToCountry.put(parts[2], parts[0]);
+                countryToCountryCode.put(parts[0], parts[2]);
+
+
+
             }
         }
         catch (IOException | URISyntaxException ex) {
@@ -57,7 +61,8 @@ public class CountryCodeConverter {
      */
     public String fromCountryCode(String code) {
         // TODO Task B: update this code to use an instance variable to return the correct value
-        return code;
+
+        return countryCodeToCountry.get(code);
     }
 
     /**
@@ -67,7 +72,7 @@ public class CountryCodeConverter {
      */
     public String fromCountry(String country) {
         // TODO Task B: update this code to use an instance variable to return the correct value
-        return country;
+        return countryToCountryCode.get(country);
     }
 
     /**
@@ -76,6 +81,6 @@ public class CountryCodeConverter {
      */
     public int getNumCountries() {
         // TODO Task B: update this code to use an instance variable to return the correct value
-        return 0;
+        return countryCodeToCountry.size();
     }
 }
