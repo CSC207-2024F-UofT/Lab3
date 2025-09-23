8a6349a728a49b4d307c4f638580235d32044cb4
Christina <rongweihu05@gmail.com>
2025-09-23 12:13:49 -0400

    completed task B 69 and 78

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -66,8 +66,8 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return country;
+        // in countryToCountryCode, find corresponding country code, return that country code
+        return this.countryCodeToCountry.get(country);
     }
 
     /**
@@ -75,7 +75,7 @@ public class CountryCodeConverter {
      * @return how many countries are included in this country code converter.
      */
     public int getNumCountries() {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return 0;
+        // find how many elements in either the code to country, or country to code
+        return countryCodeToCountry.size();
     }
 }
