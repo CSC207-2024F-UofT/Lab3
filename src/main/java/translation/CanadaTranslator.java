51ab7aaa9e44412a8a45e08480900f5476131efe
Nathan Fermin <nathan.fermin@mail.utoronto.ca>
2025-09-23 11:34:42 -0400

    New shit

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 // TODO Task 1: as a team, update this class so that it also supports the Spanish language code "es" and
-//              one more language code of your team's choice. Submit a PR once the code is working and
+//              one more language code of your team's choice. Submit a PR (pull request) once the code is working and
 //              make sure everyone has a local working copy of the code.
 
 /**
@@ -22,7 +22,7 @@ public class CanadaTranslator implements Translator {
      */
     @Override
     public List<String> getLanguageCodes() {
-        return new ArrayList<>(List.of("de", "en", "zh"));
+        return new ArrayList<>(List.of("de", "en", "zh", "es", "jp"));
     }
 
     /**
@@ -56,6 +56,12 @@ public class CanadaTranslator implements Translator {
         }
         else if ("zh".equals(languageCode)) {
             return "加拿大";
+        }
+        else if ("es".equals(languageCode)) {
+            return "Canadá";
+        }
+        else if ("jp".equals(languageCode)) {
+            return "カナダ";
         }
         else {
             return null;
