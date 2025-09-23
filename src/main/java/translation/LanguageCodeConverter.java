8d596b735414d329c0a65d9b25b90cc10904c7cc
peterluo <realpeter.luo@mail.utoronto.ca>
2025-09-22 23:33:12 -0400

    Task a done

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,13 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
+                String[] parts = line.split("\t");
+                if (parts.length >= 2) {
+                    String code = parts[0].toLowerCase();
+                    String language = parts[1];
+                    languageCodeToLanguage.put(code, language);
+                    languageToLanguageCode.put(language, code);
+                }
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -56,8 +62,8 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return code;
+        return languageCodeToLanguage.get(code);
+
     }
 
     /**
@@ -66,8 +72,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        return languageToLanguageCode.get(language);
     }
 
     /**
