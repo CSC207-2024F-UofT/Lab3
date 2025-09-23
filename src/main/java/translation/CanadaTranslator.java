66e31fd77ebecdb435f9796b3b671884ed86c078
isseijacobson <isseimjacobson@gmail.com>
2025-09-23 11:32:06 -0400

    g

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -22,7 +22,7 @@ public class CanadaTranslator implements Translator {
      */
     @Override
     public List<String> getLanguageCodes() {
-        return new ArrayList<>(List.of("de", "en", "zh", "es"));
+        return new ArrayList<>(List.of("de", "en", "zh", "es","jp"));
     }
 
     /**
