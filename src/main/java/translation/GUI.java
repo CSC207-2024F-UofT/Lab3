e925c829dc39d14991fc74550aa1d472276bb3ab
Cotaroy Cheung <twinflame265@gmail.com>
2025-09-23 15:48:35 -0400

    switching to JSONTranslator

--- src/main/java/translation/GUI.java
+++ src/main/java/translation/GUI.java
@@ -52,7 +52,7 @@ public class GUI {
                     country = countryCodeConverter.fromCountry(country);
                     // for now, just using our simple translator, but
                     // we'll need to use the real JSON version later.
-                    Translator translator = new CanadaTranslator();
+                    Translator translator = new JSONTranslator();
 
                     String result = translator.translate(country, language);
                     if (result == null) {
