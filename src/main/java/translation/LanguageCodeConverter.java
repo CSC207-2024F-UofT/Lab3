c3fed4c8d6f39a808e7911d566beae8905833d43
BPLboi <boyanlitchev@yahoo.com>
2025-09-23 11:30:06 -0400

    Finished language code conversions

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,11 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
+
+                String language_code = line.split("\t")[1];
+                String language = line.split("\t")[0];
+                languageCodeToLanguage.put(language_code, language);
+                languageToLanguageCode.put(language, language_code);
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -56,8 +60,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return code;
+        return languageCodeToLanguage.get(code);
     }
 
     /**
@@ -66,8 +69,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        return languageToLanguageCode.get(language);
     }
 
     /**
