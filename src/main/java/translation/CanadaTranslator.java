017933a3c5de2b34f9513d10cf3ebc71d677b86d
Kappapapa123 <suzuki.keishi.227@gmail.com>
2025-09-23 11:53:51 -0400

    Update class so that it supports Spanish language code "es" and Japanese "jp"

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -3,9 +3,6 @@ package translation;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO Task 1: as a team, update this class so that it also supports the Spanish language code "es" and
-//              one more language code of your team's choice. Submit a PR once the code is working and
-//              make sure everyone has a local working copy of the code.
 
 /**
  * An implementation of the Translator interface that translates
@@ -22,7 +19,7 @@ public class CanadaTranslator implements Translator {
      */
     @Override
     public List<String> getLanguageCodes() {
-        return new ArrayList<>(List.of("de", "en", "zh"));
+        return new ArrayList<>(List.of("de", "en", "zh", "es", "jp"));
     }
 
     /**
@@ -54,8 +51,14 @@ public class CanadaTranslator implements Translator {
         else if (languageCode.equals("en")) {
             return "Canada";
         }
-        else if ("zh".equals(languageCode)) {
+        else if (languageCode.equals("zh")) {
             return "加拿大";
+        }
+        else if (languageCode.equals("es")) {
+            return "Canadá";
+        }
+        else if (languageCode.equals("jp")) {
+            return "カナダ";
         }
         else {
             return null;
