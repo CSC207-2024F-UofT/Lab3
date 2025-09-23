c1a1b50f475c6506c889c4bcc9fac3594807ce5a
javifriki <3167940565@qq.com>
2025-09-23 11:38:02 -0400

    Ben's task 1

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -3,9 +3,6 @@ package translation;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO Task 1: as a team, update this class so that it also supports the Spanish language code "es" and
-//              one more language code of your team's choice. Submit a PR once the code is working and
-//              make sure everyone has a local working copy of the code.
 
 /**
  * An implementation of the Translator interface that translates
@@ -22,7 +19,7 @@ public class CanadaTranslator implements Translator {
      */
     @Override
     public List<String> getLanguageCodes() {
-        return new ArrayList<>(List.of("de", "en", "zh"));
+        return new ArrayList<>(List.of("de", "en", "zh", "es"));
     }
 
     /**
@@ -57,7 +54,9 @@ public class CanadaTranslator implements Translator {
         else if ("zh".equals(languageCode)) {
             return "加拿大";
         }
-        else {
+        else if (languageCode.equals("es")) {
+            return "Canadá";
+        } else {
             return null;
         }
     }
