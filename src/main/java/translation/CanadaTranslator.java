098932f0b305bb140d962b43bba5c36e2c3ed485
0507ellie <elliechang0328@gmail.com>
2025-09-25 19:26:51 -0400

    edited CanadaTranslator

--- src/main/java/translation/CanadaTranslator.java
+++ src/main/java/translation/CanadaTranslator.java
@@ -19,7 +19,7 @@ public class CanadaTranslator implements Translator {
      */
     @Override
     public List<String> getLanguageCodes() {
-        return new ArrayList<>(List.of("de", "en", "zh"));
+        return new ArrayList<>(List.of("de", "en", "zh", "es", "fr"));
     }
 
     /**
