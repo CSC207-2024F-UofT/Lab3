4612c1b8a574ebc6292f73f16a0e9afb159baf86
peterluo <realpeter.luo@mail.utoronto.ca>
2025-09-26 03:07:03 -0400

    CountryCodeConverter and LanguageCodeConverter committed

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -41,7 +41,6 @@ public class CountryCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
-                // TODO Task B: use parts to populate the instance variables
                 countryCodeToCountry.put(parts[2], parts[0]);
                 countryToCountryCode.put(parts[0], parts[2]);
             }
@@ -58,7 +57,6 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
         String newCode = code.toUpperCase();
         return countryCodeToCountry.get(newCode);
     }
@@ -69,7 +67,6 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
         return countryToCountryCode.get(country);
 
     }
@@ -79,7 +76,6 @@ public class CountryCodeConverter {
      * @return how many countries are included in this country code converter.
      */
     public int getNumCountries() {
-        // TODO Task B: update this code to use an instance variable to return the correct value
         return countryToCountryCode.size();
     }
 }
