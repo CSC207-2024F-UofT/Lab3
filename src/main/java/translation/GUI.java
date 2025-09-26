310be14d3e78ef24cbb6abfbe507bf8aa1dd6d5c
Luca Piccone <lpiccone19@gmail.com>
2025-09-26 15:35:52 -0400

    Updated GUI.java (Task D)

--- src/main/java/translation/GUI.java
+++ src/main/java/translation/GUI.java
@@ -1,6 +1,8 @@
 package translation;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,23 +30,32 @@ public class GUI {
                 countries.add(countryCodeConverter.fromCountryCode(countryCode));
             JList<String> jlist = new JList<>(countries.toArray(new String[0]));
             JScrollPane scrollPane = new JScrollPane(jlist);
-            languagePanel.add(new JLabel("Country:"));
             languagePanel.add(scrollPane);
 
-            JPanel buttonPanel = new JPanel();
-            JButton submit = new JButton("Submit");
-            buttonPanel.add(submit);
-
+            JPanel translationPanel = new JPanel();
             JLabel resultLabelText = new JLabel("Translation:");
-            buttonPanel.add(resultLabelText);
+            translationPanel.add(resultLabelText);
             JLabel resultLabel = new JLabel("\t\t\t\t\t\t\t");
-            buttonPanel.add(resultLabel);
+            translationPanel.add(resultLabel);
 
-
-            // adding listener for when the user clicks the submit button
-            submit.addActionListener(new ActionListener() {
+            comboBox.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
+                    String language = (String) comboBox.getSelectedItem();
+                    String country = jlist.getSelectedValue();
+                    String result = jsonTranslator.translate(
+                            countryCodeConverter.fromCountry(country),
+                            languageCodeConverter.fromLanguage(language));
+                    if (result == null) {
+                        result = "no translation found!";
+                    }
+                    resultLabel.setText(result);
+                }
+            });
+
+            jlist.addListSelectionListener(new ListSelectionListener() {
+                @Override
+                public void valueChanged(ListSelectionEvent e) {
                     String language = (String) comboBox.getSelectedItem();
                     String country = jlist.getSelectedValue();
                     String result = jsonTranslator.translate(
@@ -60,8 +71,8 @@ public class GUI {
             JPanel mainPanel = new JPanel();
             mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
             mainPanel.add(countryPanel);
+            mainPanel.add(translationPanel);
             mainPanel.add(languagePanel);
-            mainPanel.add(buttonPanel);
 
             JFrame frame = new JFrame("Country Name Translator");
             frame.setContentPane(mainPanel);
